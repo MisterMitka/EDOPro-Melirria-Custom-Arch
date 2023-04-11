7e9a22753cc8b74e0f7ffe628c5d7d216b03c733
MisterMitka <104612777+MisterMitka@users.noreply.github.com>
2023-04-11 10:28:33 +0500

    Update c987000303.lua

--- script/c987000303.lua
+++ script/c987000303.lua
@@ -49,14 +49,14 @@ end
 function s.thfilter(c)
 	return c:IsSetCard(0x3dd) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
 end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.datg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
 	e:SetLabel(g:GetFirst():GetFieldID())
 end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
+function s.daop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
