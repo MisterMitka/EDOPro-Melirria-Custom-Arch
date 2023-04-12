d69e9e715cda23fad2bb279e550989cf08f3e489
MisterMitka <104612777+MisterMitka@users.noreply.github.com>
2023-04-12 20:27:02 +0500

    Update c987000303.lua

--- script/c987000303.lua
+++ script/c987000303.lua
@@ -49,7 +49,9 @@ function s.thfilter(c)
 	return c:IsSetCard(0x3dd) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
 end
 function s.datg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) 
+end
+function s.filter(tp)
 	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
