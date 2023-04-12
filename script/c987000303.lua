517fa3842266754b62d60d50d4b54fcf486024f3
MisterMitka <104612777+MisterMitka@users.noreply.github.com>
2023-04-12 21:18:44 +0500

    Update c987000303.lua

--- script/c987000303.lua
+++ script/c987000303.lua
@@ -48,14 +48,14 @@ end
 function s.thfilter(c)
 	return c:IsSetCard(0x3dd) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
 end
-function s.filter(c)
-	return c:IsSetCard(0x3dd) and c:IsType(TYPE_MONSTER) and c:IsFaceup()
-end
 function s.datg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function s.filter(c)
+	return c:IsSetCard(0x3dd) and c:IsType(TYPE_MONSTER) and c:IsFaceup()
 end
 function s.rdcon(e)
 	local tp=e:GetHandlerPlayer()
