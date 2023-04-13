6a6fc0718a70c684edf55ad8776bf1b78ce4b439
MisterMitka <104612777+MisterMitka@users.noreply.github.com>
2023-04-13 10:04:42 +0500

    Update c987000306.lua

--- script/c987000306.lua
+++ script/c987000306.lua
@@ -27,14 +27,14 @@ s.listed_series={0x38d}
 function s.filter(c)
 	return c:IsFaceup() and c:IsCanChangePosition()
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.postg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
 	local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.posop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsFaceup() and tc:IsControler(1-tp) and tc:IsRelateToEffect(e) then
 		Duel.ChangePosition(tc,POS_FACEDOWN_DEFENSE,0,POS_FACEUP_DEFENSE,0,POS_FACEUP_ATTACK,0)
