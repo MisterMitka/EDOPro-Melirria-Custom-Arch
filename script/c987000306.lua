5fd22801b02329279d12e821bf1f1472d6d34bcf
MisterMitka <104612777+MisterMitka@users.noreply.github.com>
2023-04-28 10:48:09 +0500

    Update c987000306.lua

--- script/c987000306.lua
+++ script/c987000306.lua
@@ -3,7 +3,6 @@ local s,id=GetID()
 function s.initial_effect(c)
 --pos change then gain atk and def
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0)
 	e1:SetCategory(CATEGORY_POSITION+CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
@@ -36,7 +35,13 @@ end
 function s.filter(c)
 	return c:IsSetCard(0x38d) and c:IsType(TYPE_MONSTER) and c:IsFaceup()
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
 		local e1=Effect.CreateEffect(e:GetHandler())
