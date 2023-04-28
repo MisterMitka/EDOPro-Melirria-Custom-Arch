3b0db16fe60b9b5e34d59a7d0478775ae610ef54
MisterMitka <104612777+MisterMitka@users.noreply.github.com>
2023-04-28 16:27:32 +0500

    Update c987000306.lua

--- script/c987000306.lua
+++ script/c987000306.lua
@@ -26,6 +26,9 @@ function s.postg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
 end
+function s.atkfil(c)
+	return c:IsFaceup() and c:IsSetCard(0x38d)
+end
 function s.posop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
@@ -42,5 +45,16 @@ function s.posop(e,tp,eg,ep,ev,re,r,rp)
 		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
 		e2:SetValue(tc:GetDefense()/2)
 		tc:RegisterEffect(e2)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(s.atkfil,tp,LOCATION_MZONE,0,nil)
+	if #g>0 then
+	local sc=g:GetFirst()
+	for sc in aux.Next(g) do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(e:GetLabel())
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		sc:RegisterEffect(e1)
 end
 end
