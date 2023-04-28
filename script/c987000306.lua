0648458cd4d9715e11757a8745b6ee2ec50187a5
MisterMitka <104612777+MisterMitka@users.noreply.github.com>
2023-04-28 16:20:54 +0500

    Update c987000306.lua

--- script/c987000306.lua
+++ script/c987000306.lua
@@ -43,18 +43,4 @@ function s.posop(e,tp,eg,ep,ev,re,r,rp)
 		e2:SetValue(tc:GetDefense()/2)
 		tc:RegisterEffect(e2)
 end
-function s.atkfil(c)
-	return c:IsFaceup() and c:IsSetCard(0x38d)
-	local c=e:GetHandler()
-	local g=Duel.GetMatchingGroup(s.atkfil,tp,LOCATION_MZONE,0,nil)
-	if #g>0 then
-	local sc=g:GetFirst()
-	for sc in aux.Next(g) do
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(e:GetLabel())
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		sc:RegisterEffect(e1)
-end
 end
