86e14539199b75e536df7d1c91f5fcd5c91ad997
MisterMitka <104612777+MisterMitka@users.noreply.github.com>
2023-04-28 16:11:21 +0500

    Update c987000306.lua

--- script/c987000306.lua
+++ script/c987000306.lua
@@ -47,7 +47,7 @@ function s.posop(e,tp,eg,ep,ev,re,r,rp)
 		tc:RegisterEffect(e2)
 end
 function s.atkfil(c)
-	return c:IsFaceup() and c:IsSetCard(0x38d)
+	return c:IsFaceup() and c:IsSetCard(0x3dd)
 end
 	function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.atkfil,tp,LOCATION_MZONE,0,1,nil) end
@@ -61,15 +61,8 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(e:GetLabel())
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		e1:SetValue(e:GetAttack())
 		sc:RegisterEffect(e1)
-		local  e2=Effect.CreateEffect(e:GetHandler())
-		e2:SetOwnerPlayer(a)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_UPDATE_DEFENSE)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		e2:SetValue(e:GetDefense())
-		sc:RegisterEffect(e2)
-	end
+end
 end
