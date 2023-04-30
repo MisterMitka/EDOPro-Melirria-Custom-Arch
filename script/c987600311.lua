12a5a57735f8ab0f73a40597a6921fd4e4023c11
MisterMitka <104612777+MisterMitka@users.noreply.github.com>
2023-04-30 12:23:54 +0500

    Update c987600311.lua

--- script/c987600311.lua
+++ script/c987600311.lua
@@ -49,7 +49,7 @@ function s.thfilter(c)
 	return c:IsSetCard(0x3dd) and c:IsAbleToDeck()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil) 
+	if chk==0 then return Duel.IsExistingMatchingCard(tp,s.thfilter,LOCATION_GRAVE,0,1,1,nil) 
 		and Duel.IsPlayerCanSendtoDeck(tp,nil,1,REASON_EFFECT)~=0 end
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_GRAVE)
 	Duel.SetOperationInfo(0,CATEGORY_SEARCH,nil,0,tp,1)
